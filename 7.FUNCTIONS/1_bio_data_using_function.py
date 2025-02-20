ee4f8975f089e2df4958f28cf503732398033d70
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-02-20 13:43:24 +0530

    Update 1_bio_data_using_function.py

---
+++ 7.FUNCTIONS/1_bio_data_using_function.py
@@ -0,0 +1,18 @@
+# display biodata using function
+
+def bioData() :
+    name=input("Your name : ")
+    rollNo=input("your roll no : ")
+    contactNo =input("your contact no : ")
+    address=input("Your address : ")
+
+    print()
+    print()
+
+    print("your name is ",name)
+    print("your roll number is ",rollNo)
+    print("your contact number is ",contactNo)
+    print("your address is ",address)
+
+# function calling
+bioData()
