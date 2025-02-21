b1428d2a48bbe6325f0672c05234bc1a60429daf
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-02-21 22:31:08 +0530

    Update 4_returning_multiple_valuess.py

--- 7.FUNCTIONS/4_returning_multiple_valuess.py
+++ 7.FUNCTIONS/4_returning_multiple_valuess.py
@@ -1,16 +1,22 @@
+# function returning multiple values 
 
+# defining function
 def calc(a,b):
     sum=a+b
     sub=a-b
     mul=a*b
     div=a/b
-    return sum,sub,mul,div
+    return sum,sub,mul,div     # returning multiple values
 
-t=calc(100,50)
+t=calc(100,50)    # as a tuple
 
 print(t)
 print(type(t))
 print("The Results are:")
 
+# accessing values from tuple using loop
 for i in t:
-    print(i)
+    print(i)                 
+
+
+    
