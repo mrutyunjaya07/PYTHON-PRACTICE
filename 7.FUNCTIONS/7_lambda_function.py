dcd02eb5039b5a5d4e47b0dca64a7b72e41421d5
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-04-16 17:15:25 +0530

    update

---
+++ 7.FUNCTIONS/7_lambda_function.py
@@ -0,0 +1,23 @@
+# Lambda function
+
+# a function without name is called as anonymous functions or lamda functions.
+
+# used for instant use(i.e for one time usage)
+
+# syntax
+
+   # lambda argument_list:expression
+
+square= lambda n:n*n
+
+print(square(5))
+print(square(90))
+print(square(-53))
+
+
+sum= lambda a,b:a+b
+
+print(sum(90,56))
+print(sum(-89,55))
+
+# no need of return statement, it returns internally
