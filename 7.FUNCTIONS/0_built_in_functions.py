7fdaf8c1ba89c20bda1a1149817292553464a442
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-04-16 16:04:49 +0530

    Create 0_built_in_functions.py

---
+++ 7.FUNCTIONS/0_built_in_functions.py
@@ -0,0 +1,15 @@
+# functions which are available with python module implicitly are known as built-in functions
+
+var="MJ"
+
+# print() is also a built in function
+# id() gives the memory address of the variable
+print(id(var))
+
+#type( ) gives the data type of the variable
+
+print(type(var))
+
+#eval() gives the mathematical evaluation of an expression
+# takes string input
+print(eval("10+30-2*90"))
