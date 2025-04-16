6599672a920eceb6f1d7c741c0bb2d22d4da1fe3
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-04-16 17:47:22 +0530

    add

--- 7.FUNCTIONS/8_use_of_lambda_function.py
+++ 7.FUNCTIONS/8_use_of_lambda_function.py
@@ -4,6 +4,10 @@
 # with filter(), map(), reduce() functions as they excepts function as arguments
 
 
+# filter(function, sequence)
+      # function should return True or False
+      # gives the elemnts following the function
+
 # without using lambda function
 def isEven(num):
     if num%2==0:
@@ -27,3 +31,32 @@ l2=list(filter(lambda n:n%2!=0,l))
 print(l1,l2)
 
 
+
+# map(function, sequence)
+  # gives the new elemnts based on function
+
+l1=[2,6,3,15,9]
+
+l2=list(map(lambda n:2*n,l1))
+
+print(l2)
+
+
+# both list should be of same size
+l3=[2,5,3,1]
+l4=[1,0,5,8]
+
+l5=list(map(lambda a,b:a*b,l3,l4))
+print(l5)
+
+
+
+
+# reduce( )
+   #reduces sequence of elements into a single element by applying the specified function.
+   # import functools module
+
+from functools import *
+l=[10,20,30,40,50]
+result=reduce(lambda x,y:x+y,l)
+print(result)
---
+++ 7.FUNCTIONS/9_nested_functions.py
@@ -0,0 +1,15 @@
+
+# it is basically function inside functon
+
+def function1( ):
+
+    pass
+
+    def function2( ):
+        pass
+
+
+    return
+
+
+function1( )
