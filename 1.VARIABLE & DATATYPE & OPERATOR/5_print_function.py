42d73bfa8e36693e4c05c36d474d7554bf6944ed
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-07-29 20:00:43 +0530

    update

---
+++ 1.VARIABLE & DATATYPE & OPERATOR/5_print_function.py
@@ -0,0 +1,11 @@
+
+#print() is a function
+# can take "" or ''
+print("Hello World")
+print('Hay MJ')
+
+print("MJ print('HY')")
+
+print("Hello world\nHello WOrld\nHello World") #used for newline
+print()   #blank space
+print("Hello"+" World")   # concatenation
